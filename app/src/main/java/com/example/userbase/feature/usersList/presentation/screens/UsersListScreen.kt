ca511250dd4231ab1f8f2ae5df2f5ec384704c3f
Toka <tokaaliamien@gmail.com>
2025-09-13 21:00:17 +0300

    [Add] error and empty user list logic

--- app/src/main/java/com/example/userbase/feature/usersList/presentation/screens/UsersListScreen.kt
+++ app/src/main/java/com/example/userbase/feature/usersList/presentation/screens/UsersListScreen.kt
@@ -1,20 +1,22 @@
 package com.example.userbase.feature.usersList.presentation.screens
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.res.stringResource
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import com.example.userbase.feature.common.presentation.models.GenderUIModel
-import com.example.userbase.feature.common.presentation.models.UserUIModel
+import com.example.userbase.R
 import com.example.userbase.feature.usersList.presentation.screens.actions.UsersListUiActions
 import com.example.userbase.feature.usersList.presentation.screens.components.AddUserButton
 import com.example.userbase.feature.usersList.presentation.screens.components.UserItem
@@ -43,54 +45,40 @@ fun UsersListContent(
     uiState: UserListUiState,
     modifier: Modifier = Modifier,
 ) {
-    val users = listOf(
-        UserUIModel(
-            1, "Jane Doe", age = 29, jobTitle = "Teacher",
-            genderUIModel = GenderUIModel.FEMALE
-        ), UserUIModel(
-            1, "John Doe", age = 25, jobTitle = "Software Engineer",
-            genderUIModel = GenderUIModel.MALE
-        ), UserUIModel(
-            1, "John Doe", age = 20, jobTitle = "Software Engineer",
-            genderUIModel = GenderUIModel.MALE
-        ), UserUIModel(
-            1, "John Doe", age = 20, jobTitle = "Software Engineer",
-            genderUIModel = GenderUIModel.MALE
-        )
-    )
-
     val dimens = LocalDimensions.current
-    LazyColumn(
-        contentPadding = innerPadding,
-        modifier = modifier.padding(dimens.large),
-        verticalArrangement = Arrangement.spacedBy(dimens.large)
-    ) {
-        items(items = uiState.users) { user ->
-            UserItem(user)
+    if (uiState.users.isEmpty())
+        UsersListPlaceHolder()
+    if (uiState.error != null)
+        UsersListError(uiState.error)
+    else {
+        LazyColumn(
+            contentPadding = innerPadding,
+            modifier = modifier.padding(dimens.large),
+            verticalArrangement = Arrangement.spacedBy(dimens.large)
+        ) {
+            items(items = uiState.users) { user ->
+                UserItem(user)
+            }
         }
     }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun UsersListScreenPreview() {
-    UserBaseTheme {
-        val users = listOf(
-            UserUIModel(
-                1, "Jane Doe", age = 29, jobTitle = "Teacher",
-                genderUIModel = GenderUIModel.FEMALE
-            ), UserUIModel(
-                1, "John Doe", age = 25, jobTitle = "Software Engineer",
-                genderUIModel = GenderUIModel.MALE
-            ), UserUIModel(
-                1, "John Doe", age = 20, jobTitle = "Software Engineer",
-                genderUIModel = GenderUIModel.MALE
-            ), UserUIModel(
-                1, "John Doe", age = 20, jobTitle = "Software Engineer",
-                genderUIModel = GenderUIModel.MALE
-            )
+fun UsersListPlaceHolder() {
+    Box {
+        Text(
+            text = stringResource(R.string.no_users_found),
+            modifier = Modifier.align(Alignment.Center)
         )
+    }
+}
 
-        UsersListScreen()
+@Composable
+fun UsersListError(errorMessage: String) {
+    Box {
+        Text(
+            text = errorMessage,
+            modifier = Modifier.align(Alignment.Center)
+        )
     }
 }
